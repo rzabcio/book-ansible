ce95434bf42f5fa61d2112d3d17fd5333f66f36f
Jakub Głazik <jglazik@sygnity.pl>
2021-11-06 20:24:50 +0100

    Chapter 2: Nginx examples.

--- 00-gcp/createHosts.sh
+++ 00-gcp/createHosts.sh
@@ -9,13 +9,13 @@ ANSIBLE3_IP=`gcloud compute instances describe ansible3 --format="get(networkInt
 
 # echo "IPs: $ANSIBLE1_IP, $ANSIBLE2_IP, $ANSIBLE3_IP"
 if [ -n $ANSIBLE1_IP ]; then
-	sed -i "s/ansible1 .*/ansible1 ansible_host=$ANSIBLE1_IP/" $HOSTS_FILE
+	sed -i "s/ansible1 ansible_host=.*/ansible1 ansible_host=$ANSIBLE1_IP/" $HOSTS_FILE
 fi
 if [ -n $ANSIBLE2_IP ]; then
-	sed -i "s/ansible2 .*/ansible2 ansible_host=$ANSIBLE2_IP/" $HOSTS_FILE
+	sed -i "s/ansible2 ansible_host=.*/ansible2 ansible_host=$ANSIBLE2_IP/" $HOSTS_FILE
 fi
 if [ -n $ANSIBLE3_IP ]; then
-	sed -i "s/ansible3 .*/ansible3 ansible_host=$ANSIBLE3_IP/" $HOSTS_FILE
+	sed -i "s/ansible3 ansible_host=.*/ansible3 ansible_host=$ANSIBLE3_IP/" $HOSTS_FILE
 fi
 
 # cat $HOSTS_FILE
